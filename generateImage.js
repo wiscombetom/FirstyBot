24f77a1211a75c60d9201679acd08e9b9f324c78
Tom Wiscombe <wiscombetom@gmail.com>
2022-06-02 06:43:27 -0400

    Added welcome image generator

---
+++ generateImage.js
@@ -0,0 +1,57 @@
+const Canvas = require("canvas")
+const Discord = require("discord.js")
+
+const canvasDimensions = {
+	width: 1200,
+	height: 675,
+	margin: 50
+}
+
+const avatar = {
+	size: 256,
+	x: 480,
+	y: 170
+}
+
+const generateImage = async (member) => {
+	const username = member.user.username
+	const userdisc = member.user.discriminator
+	const useravat = member.user.displayAvatarURL({
+		format: "png",
+		dynamic: false,
+		size: avatar.size
+	})
+	const bgimgurl = "https://images.pexels.com/photos/126271/pexels-photo-126271.jpeg"
+	const canvas = Canvas.createCanvas(canvasDimensions.width, canvasDimensions.height)
+	const ctx = canvas.getContext("2d")
+	const bgimgobj = await Canvas.loadImage(bgimgurl)
+	const avimgobj = await Canvas.loadImage(useravat)
+	
+	ctx.drawImage(bgimgobj, 0, 0)
+	ctx.fillStyle = "rgba(0, 0, 0, 0.8)"
+	ctx.fillRect(canvasDimensions.margin, canvasDimensions.margin, canvasDimensions.width - canvasDimensions.margin * 2, canvasDimensions.height - canvasDimensions.margin * 2)
+	ctx.save()
+	ctx.beginPath()
+	ctx.arc(avatar.x + avatar.size / 2, avatar.y + avatar.size / 2, avatar.size / 2, 0, Math.PI * 2, true)
+	ctx.closePath()
+	ctx.clip()
+	ctx.drawImage(avimgobj, avatar.x, avatar.y)
+	ctx.restore()
+
+	ctx.fillStyle = "white"
+	ctx.textAlign = "center"
+
+	ctx.font = "50px Sans"
+	ctx.fillText(`Welcome`, canvasDimensions.width / 2, canvasDimensions.margin + 70)
+	
+	ctx.font = "60px Sans"
+	ctx.fillText(`${username}#${userdisc}`, canvasDimensions.width / 2, canvasDimensions.height - canvasDimensions.margin - 125)
+	
+	ctx.font = "40px Sans"
+	ctx.fillText(`to the server`, canvasDimensions.width / 2, canvasDimensions.height - canvasDimensions.margin - 50)
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+	return attachment
+}
+
+module.exports = generateImage
--- index.js
+++ index.js
@@ -1,20 +1,15 @@
+const generateImage = require("./generateImage")
 const Discord = require("discord.js")
 require("dotenv").config()
-// const fs = require('fs')
-// const os = require('node:os')
-
-// var token = fs.readFileSync('bot-token.txt', 'utf-8')
+const welcomeChannelId = "981579348970708992"
 
 var client = new Discord.Client({
 	intents: [
 		"GUILDS",
-		"GUILD_MESSAGES"
+		"GUILD_MESSAGES",
+		"GUILD_MEMBERS"
 	]
 })
-
-// client.on("ready", function(){
-// 	console.log(`Logged in as ${client.user.tag}`)
-// })
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}`)
@@ -23,8 +18,31 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
 	console.log(`${message.createdAt}: "${message.content}" sent by '${message.author.tag}'`)
 	if (message.content == "!hw") {
-		message.reply("Hello World!")
+		reply_message(message, "Hello World!")
+	}
+	if (message.content == "!twm") {
+		send_welcome_message(message.guild, message.member)
 	}
 })
+
+client.on("guildMemberAdd", async (member) => {
+	await send_welcome_message(member.guild, member)
+})
+
+function reply_message(original, message) {
+	original.reply(message)
+}
+
+async function send_welcome_message(guild, member) {
+	const welcomeImage = await generateImage(member)
+	const welcomeChannel = guild.channels.cache.get(welcomeChannelId)
+	const welcomeMessage = `<@${member.id}> Welcome to the server!`
+	welcomeChannel.send({
+		content: welcomeMessage,
+		files: [
+			welcomeImage
+		]
+	})
+}
 
 client.login(process.env.TOKEN)
